a799971741709fbf107fd2aa2039d21bc092e139
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2024-11-10 19:18:26 +0100

    evolve godon breeder seeder image

--- godon-seeder/godon_seeder.sh
+++ godon-seeder/godon_seeder.sh
@@ -6,4 +6,14 @@ set -eEux
 set -o pipefail
 shopt -s inherit_errexit
 
-echo "Implement me"
+echo "Seeding from ${GODON_VERSION}"
+
+pushd "${GODON_DIR}"
+git checkout -B "${GODON_VERSION}" "${GODON_VERSION}"
+
+for breeder in $(ls -1 "breeder")
+do
+    echo "## performing seeding for ${breeder}"
+    echo "## ${breeder} . DONE"
+done
+
