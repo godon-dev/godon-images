d5da8da9a3e6256d46488b1d5fe50a42221a0709
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2024-11-10 19:06:44 +0100

    start with seeder container and script

---
+++ godon-seeder/godon_seeder.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Script that seeds the breeder flows and scripts into the windmill orchestration engine.
+
+set -eEux
+set -o pipefail
+shopt -s inherit_errexit
+
+echo "Implement me"
