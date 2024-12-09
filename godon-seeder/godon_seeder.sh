26569f98149e03a44d5017aba3899c10533106d3
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2024-12-09 09:02:13 +0100

    godon_seeder - minimal push of controller scripts

--- godon-seeder/godon_seeder.sh
+++ godon-seeder/godon_seeder.sh
@@ -6,14 +6,35 @@ set -eEux
 set -o pipefail
 shopt -s inherit_errexit
 
-echo "Seeding from ${GODON_VERSION}"
+## Logging in to Windmill to attain token
+export WMILL_TOKEN="$(curl https://app.windmill.dev/api/auth/login \
+                      --request POST \
+                      --header 'Content-Type: application/json' \
+                      --data '{
+                      "email": "admin@windmill.dev",
+                      "password": "changeme"
+                       }')"
 
+## Set Default Windmill Workspace
+export WMILL_WORKSPACE="godon"
+
+## Clone and Checkout the Relevant Scripts and Flows Version
+echo "Seeding from ${GODON_VERSION}"
 pushd "${GODON_DIR}"
 git checkout -B "${GODON_VERSION}" "${GODON_VERSION}"
 
-for breeder in $(ls -1 "breeder")
+echo "Creating godon logic workspace"
+wmill workspace add "${WMILL_WORKSPACE}" "${WMILL_WORKSPACE}" "https://app.windmill.dev/godon"
+
+pushd controller
+for script in $(ls -1)
 do
-    echo "## performing seeding for ${breeder}"
-    echo "## ${breeder} . DONE"
+    echo "## performing seeding for controller logic"
+    wmill script bootstrap --summary "${script}" \
+                           --description "${script}" \
+                           "${script}" "python"
+
+    wmill script push "${script}"
+    echo "## Controller ... DONE"
 done
 
