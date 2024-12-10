ff1facc0a347d1b3426425817387fec03cec3bea
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2024-12-10 13:02:29 +0100

    godon seeder - update seeder script after test
    Manual tests performed for exampe scripts.
    That is minimally proven working.

--- godon-seeder/godon_seeder.sh
+++ godon-seeder/godon_seeder.sh
@@ -7,7 +7,7 @@ set -o pipefail
 shopt -s inherit_errexit
 
 ## Logging in to Windmill to attain token
-export WMILL_TOKEN="$(curl https://app.windmill.dev/api/auth/login \
+export WMILL_TOKEN="$(curl godon-windmill_server-1:8000/api/auth/login \
                       --request POST \
                       --header 'Content-Type: application/json' \
                       --data '{
@@ -18,23 +18,34 @@ export WMILL_TOKEN="$(curl https://app.windmill.dev/api/auth/login \
 ## Set Default Windmill Workspace
 export WMILL_WORKSPACE="godon"
 
+## Set Windmill API Base URL
+export WMILL_BASE_URL="http://godon-windmill_server-1:8000"
+
 ## Clone and Checkout the Relevant Scripts and Flows Version
 echo "Seeding from ${GODON_VERSION}"
 pushd "${GODON_DIR}"
 git checkout -B "${GODON_VERSION}" "${GODON_VERSION}"
 
 echo "Creating godon logic workspace"
-wmill workspace add "${WMILL_WORKSPACE}" "${WMILL_WORKSPACE}" "https://app.windmill.dev/godon"
+wmill --base-url "${WMILL_BASE_URL}" --token "${WMILL_TOKEN}" --workspace "${WMILL_WORKSPACE}" workspace add "${WMILL_WORKSPACE}" "${WMILL_WORKSPACE}" "http://godon-windmill_server-1"
 
+
+### Seed Controller Logic ###
 pushd controller
-for script in $(ls -1)
+wmill init
+
+# create controller folder
+mkdir -p f/controller
+
+for script in $(ls -1 *.py)
 do
     echo "## performing seeding for controller logic"
-    wmill script bootstrap --summary "${script}" \
-                           --description "${script}" \
-                           "${script}" "python"
 
-    wmill script push "${script}"
+    mv "${script}" f/controller
+
+    wmill --base-url "${WMILL_BASE_URL}" --token "${WMILL_TOKEN}" --workspace "${WMILL_WORKSPACE}" \
+          script push f/controller/${script}
+
     echo "## Controller ... DONE"
 done
 
